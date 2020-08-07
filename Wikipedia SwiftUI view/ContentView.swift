ffcc8514a95ad98f64778805b78b7211762a7345
ArthurPauwels <arthur.pauwels@student.hogent.be>
2020-08-07 14:17:19 +0200

    Initial Commit

---
+++ Wikipedia SwiftUI view/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wikipedia SwiftUI view
+//
+//  Created by Arthur Pauwels on 07/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wikipedia SwiftUI view/Wikipedia_SwiftUI_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wikipedia_SwiftUI_viewApp.swift
+//  Wikipedia SwiftUI view
+//
+//  Created by Arthur Pauwels on 07/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct Wikipedia_SwiftUI_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
